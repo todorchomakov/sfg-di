bca8fbd66fea44ac8e950d12812ee6f2077aff23
todorchomakov <todorchomakov@gmail.com>
2021-03-11 23:16:07 +0200

    Init context example

---
+++ src/main/java/com/example/sfgdi/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.example.sfgdi.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello, World!!!");
+        return "Hi, Folks!";
+    }
+}
