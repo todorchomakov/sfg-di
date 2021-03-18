01ece85fe184e034e57f5dfe1146518eaba1cb87
todorchomakov <todorchomakov@gmail.com>
2021-03-18 21:39:45 +0200

    Init di-assignment example

---
+++ src/main/java/com/example/sfgdi/controller/PetController.java
@@ -0,0 +1,19 @@
+package com.example.sfgdi.controller;
+
+import com.example.sfgdi.service.PetService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
+
---
+++ src/main/java/com/example/sfgdi/service/CatPetService.java
@@ -0,0 +1,14 @@
+package com.example.sfgdi.service;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("cat")
+@Service
+public class CatPetService implements PetService {
+
+    @Override
+    public String getPetType() {
+        return "Cats Are the Best!";
+    }
+}
