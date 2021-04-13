fdcb8df7b14961aead2edcf952ab66b827f3aca8
todorchomakov <todorchomakov@gmail.com>
2021-04-13 23:56:41 +0300

    Init primary-profile example

--- src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,31 @@
 package com.example.sfgdi.config;
 
-import com.example.sfgdi.service.ConstructorGreetingService;
-import com.example.sfgdi.service.PropertyGreetingService;
-import com.example.sfgdi.service.SetterGreetingService;
+import com.example.sfgdi.service.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
--- src/main/java/com/example/sfgdi/service/I18nEnglishGreetingService.java
+++ src/main/java/com/example/sfgdi/service/I18nEnglishGreetingService.java
@@ -1,10 +1,5 @@
 package com.example.sfgdi.service;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-
-@Profile("EN")
-@Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService {
 
     @Override
