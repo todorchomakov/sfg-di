1f0898c86e0ec838524c7dc2775cc84884ab3b59
todorchomakov <todorchomakov@gmail.com>
2021-04-18 18:11:11 +0300

    Init scope-demo example

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,6 +1,8 @@
 package com.example.sfgdi;
 
 import com.example.sfgdi.controller.*;
+import com.example.sfgdi.service.PrototypeBean;
+import com.example.sfgdi.service.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -37,6 +39,18 @@ public class SfgDiApplication {
 		System.out.println("---------- Constructor");
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("---------- Bean Scopes ----------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
+
 	}
 
 }
---
+++ src/main/java/com/example/sfgdi/service/SingletonBean.java
@@ -0,0 +1,15 @@
+package com.example.sfgdi.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SingletonBean {
+
+    public SingletonBean() {
+        System.out.println("Creating a Singleton bean!");
+    }
+
+    public String getMyScope() {
+        return "I`m a Singleton";
+    }
+}
