7e6639b36aa6cdc6ba5049b58f645253e3731145
todorchomakov <todorchomakov@gmail.com>
2021-04-22 09:50:18 +0200

    Init application-properties example

--- src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -9,7 +9,6 @@ import com.example.sfgdi.service.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
 @ImportResource("classpath:sfgdi-config.xml")
 @Configuration
 public class GreetingServiceConfig {
